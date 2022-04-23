d21dda5aa758ab8909fa06b6797c710fbd00de4c
Kernel Soe <kernelsoe@gmail.com>
2022-04-23 22:36:55 +0900

    add ets table

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -8,11 +8,18 @@ defmodule KV.Registry do
   # end
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    server = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server, opts)
   end
 
   def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+    # Using ETS
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
+
+    # GenServer.call(server, {:lookup, name})
   end
 
   def create(server, name) do
@@ -22,38 +29,53 @@ defmodule KV.Registry do
   # Call backs
 
   @impl true
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    # names = %{}
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
 
     {:ok, {names, refs}}
   end
 
-  @impl true
-  def handle_call({:lookup, name}, _from, state) do
-    {names, _} = state
-    {:reply, Map.fetch(names, name), state}
-  end
+  # @impl true
+  # def handle_call({:lookup, name}, _from, state) do
+  #   {names, _} = state
+  #   {:reply, Map.fetch(names, name), state}
+  # end
 
   @impl true
   def handle_call({:create, name}, _from, {names, refs}) do
-    if Map.has_key?(names, name) do
-      {:reply, :duplicate, {names, refs}}
-    else
-      {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
 
-      ref = Process.monitor(bucket)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, bucket)
+      :error ->
+        {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
 
-      {:reply, :created, {names, refs}}
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
     end
+
+    # if Map.has_key?(names, name) do
+    #   {:reply, :duplicate, {names, refs}}
+    # else
+    #   {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+
+    #   ref = Process.monitor(bucket)
+    #   refs = Map.put(refs, ref, name)
+    #   names = Map.put(names, name, bucket)
+
+    #   {:reply, :created, {names, refs}}
+    # end
   end
 
   @impl true
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
+    :ets.delete(names, name)
+    # names = Map.delete(names, name)
     {:noreply, {names, refs}}
   end
 
--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,25 +1,30 @@
 defmodule RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawn buckets", %{registry: registry} do
     IO.inspect(registry)
     assert KV.Registry.lookup(registry, "shopping") == :error
-    assert :created = KV.Registry.create(registry, "shopping")
-    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    assert _pid = KV.Registry.create(registry, "shopping")
+    assert {:ok, pid} = KV.Registry.lookup(registry, "shopping")
 
-    KV.Bucket.put(bucket, "milk", 3)
-    assert KV.Bucket.get(bucket, "milk") == 3
+    KV.Bucket.put(pid, "milk", 3)
+    assert KV.Bucket.get(pid, "milk") == 3
   end
 
   test "removes buckets on exit", %{registry: registry} do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+
+    # ets operations are async
+    # so the after Agent.stop, ets might not finished hence race condition
+    # Do a call (blocking the next line) to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
@@ -29,6 +34,19 @@ defmodule RegistryTest do
 
     # Stop the bucket with msg other than :normal to crash all linked processes
     Agent.stop(bucket, :shutdown)
+
+    # Do a call (blocking the next line) to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Simulate a bucket crash by explicitly and synchronously shutting it down
+    Agent.stop(bucket, :shutdown)
+
+    catch_exit(KV.Bucket.put(bucket, "milk", 3))
   end
 end
